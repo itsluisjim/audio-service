e38098d41098de55bc37df71f8fe62762beee388
Gonzalo Vilaseca <gonzalo.vilaseca@thirdbridge.com>
2025-10-31 15:41:18 +0000

    Changes

--- src/services/AudioServices.ts
+++ src/services/AudioServices.ts
@@ -2,40 +2,35 @@ import { ServerResponse, IncomingMessage } from "http";
 
 // This is a service file, it shoul dknow nothing about requests and responses, that's the controller responsiblity, a service contains business logic that can be reused
 //  if it's bounded to requests and response it's less reusable, so I'd refactor these methods to receive just the data they need
-export const getAudioFromAwsS3Bucket = (
-  req: IncomingMessage,
-  res: ServerResponse,
-  fileId: string,
-) => {
-  console.log("Fetching audio file..");
-  console.log("Audio file found..");
-  console.log("Compressing file...");
-  console.log("Finished...");
 
-  return res.end(
-    JSON.stringify({
-      fileId,
-      fileData: "UWOVU23KJNB2KB2N3K4BKNKJ2N342",
-    }),
-  );
-};
+export const getAudioFromAwsS3Bucket = (req: IncomingMessage, res: ServerResponse, fileId: string) =>  {
 
-export const uploadAudioToAwsS3Bucket = (
-  req: IncomingMessage,
-  res: ServerResponse,
-  fileId: string,
-  filename: string,
-) => {
-  console.log("Fetching audio file..");
-  console.log("Audio file found..");
-  console.log("Compressing file...");
-  console.log("Finished...");
+    console.log("Fetching audio file..");
+    console.log("Audio file found..");
+    console.log("Compressing file...");
+    console.log("Finished...");
 
-  return res.end(
-    JSON.stringify({
-      fileId,
-      message: "Upload Successful",
-      filename,
-    }),
-  );
-};
+    return res.end(
+        JSON.stringify({
+            fileId,
+            fileData: "UWOVU23KJNB2KB2N3K4BKNKJ2N342"
+        })
+    )
+
+}
+
+export const uploadAudioToAwsS3Bucket = (req: IncomingMessage, res: ServerResponse, fileId: string, filename: string) =>  {
+
+    console.log("Fetching audio file..");
+    console.log("Audio file found..");
+    console.log("Compressing file...");
+    console.log("Finished...");
+
+    return res.end(
+        JSON.stringify({
+            fileId,
+            message: "Upload Successful",
+            filename
+        })
+    )
+}
