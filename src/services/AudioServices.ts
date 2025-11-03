a6807f8a416d5c11aed5316ba404704e57960f6f
itsluisjim <jimenezluis..lj85@gmail.com>
2025-11-03 10:33:45 -0600

    remove old code

--- src/services/AudioServices.ts
+++ src/services/AudioServices.ts
@@ -19,38 +19,4 @@ export class AudioService {
             filename
         };
     }
-
 }
-
-
-// export const getAudioFromAwsS3Bucket = (req: IncomingMessage, res: ServerResponse, fileId: string) =>  {
-
-//     console.log("Fetching audio file..");
-//     console.log("Audio file found..");
-//     console.log("Compressing file...");
-//     console.log("Finished...");
-
-//     return res.end(
-//         JSON.stringify({
-//             fileId,
-//             fileData: "UWOVU23KJNB2KB2N3K4BKNKJ2N342"
-//         })
-//     )
-
-// }
-
-// export const uploadAudioToAwsS3Bucket = (req: IncomingMessage, res: ServerResponse, fileId: string, filename: string) =>  {
-
-//     console.log("Fetching audio file..");
-//     console.log("Audio file found..");
-//     console.log("Compressing file...");
-//     console.log("Finished...");
-
-//     return res.end(
-//         JSON.stringify({
-//             fileId,
-//             message: "Upload Successful",
-//             filename
-//         })
-//     )
-// }
