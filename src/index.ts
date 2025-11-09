0579b6c5a98a8b6d0d9d114e2978e2c49f99151a
itsluisjim <jimenezluis..lj85@gmail.com>
2025-11-09 10:48:14 -0600

    inject S3 client and improve formatting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import {AppServer} from "./server";
-import {AudioServiceController} from "./controllers/AudioControllers";
-import {AudioService} from "./services/AudioServices";
+import { AppServer } from "./server";
+import { AudioServiceController } from "./controllers/AudioControllers";
+import { AudioService } from "./services/AudioServices";
+import { S3Client } from "@aws-sdk/client-s3";
 
+const s3Client = new S3Client([
+  {
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  },
+]);
+const audioService = new AudioService(s3Client);
+const audioServiceController = new AudioServiceController(audioService);
 
-const app = new AppServer(3000, new AudioServiceController(new AudioService()));
+const app = new AppServer(3000, audioServiceController);
 app.start();
