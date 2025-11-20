ffde397b0a71dd44a6142242da20c497970989a6
itsluisjim <jimenezluis..lj85@gmail.com>
2025-11-20 12:08:20 -0600

    add logic to transcribe audio

--- src/controllers/AudioControllers.ts
+++ src/controllers/AudioControllers.ts
@@ -4,12 +4,13 @@ import formidable from "formidable";
 import { pipeline, Readable } from "stream";
 import { promisify } from "util";
 import { injectable } from "tsyringe";
+import { TranscribeClient, StartTranscriptionJobCommand, StartTranscriptionJobCommandInput } from "@aws-sdk/client-transcribe";
 
 const streamPipeline = promisify(pipeline);
 
 @injectable()
 export class AudioServiceController {
-    constructor(private readonly audioRepository: AudioRepository) {}
+    constructor(private readonly audioRepository: AudioRepository, private transcribeClient: TranscribeClient) {}
 
     public async getAudio(req: any, res: any, audioFileUUID: string) {
         
@@ -55,8 +56,23 @@ export class AudioServiceController {
 
             const uploadResult = await this.audioRepository.uploadAudioToAwsS3Bucket(audioFileUUID, audio);
 
+            const commandInput: StartTranscriptionJobCommandInput = {
+              TranscriptionJobName: audioFileUUID,
+              LanguageCode: "en-US",
+              Media: {
+                MediaFileUri:
+                  `s3://${process.env.AWS_S3_BUCKET}/${audioFileUUID}.mp3`,
+              },
+              OutputBucketName: process.env.AWS_S3_TRANSCRIPT_BUCKET,
+            };
+
+            const transcribeCommand = new StartTranscriptionJobCommand(commandInput);
+
+            const transcribeResult = await this.transcribeClient.send(transcribeCommand);
+
+
             return res.end(
-                JSON.stringify({ uploadResult })
+                JSON.stringify({ uploadResult, transcribeResult })
             );
         });
     }
